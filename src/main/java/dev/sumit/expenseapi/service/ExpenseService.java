77dad364b7653993d70a10208d7dacc8e2013e49
Sumit-Rodge <sumitrodge27@gmail.com>
2024-04-28 23:11:36 +0530

    .

---
+++ src/main/java/dev/sumit/expenseapi/service/ExpenseService.java
@@ -0,0 +1,5 @@
+package dev.sumit.expenseapi.service;
+
+public interface ExpenseService {
+
+}
---
+++ src/main/java/dev/sumit/expenseapi/service/ExpenseServiceImpl.java
@@ -0,0 +1,14 @@
+package dev.sumit.expenseapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+public class ExpenseServiceImpl implements ExpenseService{
+
+//    @Autowired
+//    MongoTemplate mongoTemplate;
+
+//    public void temp(){
+//        mongoTemplate.insert("hi");
+//    }
+}
