456e63066b99bfa3280673939f17f2cc7b3aaad7
Sumit-Rodge <sumitrodge27@gmail.com>
2024-06-19 23:00:44 +0530

    final commit

--- src/main/java/dev/sumit/expenseapi/repository/ExpenseRepository.java
+++ src/main/java/dev/sumit/expenseapi/repository/ExpenseRepository.java
@@ -1,9 +1,6 @@
 package dev.sumit.expenseapi.repository;
 
 import dev.sumit.expenseapi.model.Expense;
-import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
